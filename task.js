74848ee52cd28379cd89c9aa30c8994177030de4
amreesh1214 <116099919+amreesh1214@users.noreply.github.com>
2022-12-01 10:46:40 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,47 @@
+
+let time=document.getElementById("a")
+setTimeout(() => {
+console.log(10);
+time.innerHTML=10;
+ setTimeout(() => {
+ console.log(9);
+ time.innerHTML=9;
+setTimeout(() => {
+console.log(8);
+time.innerHTML=8;
+setTimeout(() => {
+console.log(7);
+time.innerHTML=7;
+ setTimeout(() => {
+console.log(6);
+time.innerHTML=6;
+ setTimeout(() => {
+ console.log(5);
+ time.innerHTML=5;
+ setTimeout(() => {
+console.log(4);
+time.innerHTML=4;
+setTimeout(() => {
+ console.log(3);
+ time.innerHTML=3;
+ setTimeout(() => {
+ console.log(2);
+ time.innerHTML=2;
+ setTimeout(() => {
+ console.log(1);
+ time.innerHTML=1;
+ setTimeout(() => {
+ console.log('Happy Independence Day');
+ time.innerHTML="Happy Independence Day";
+ 
+ }, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+ }, 1000);
+ }, 1000);
+}, 1000);
+}, 1000);
